8f29869583ec221d4143241b0cbf3746d86fbffa
asimsca <asimsca@gmail.com>
2025-07-11 03:23:25 +0500

    changes

--- CV.Frontend/src/app/app.module.ts
+++ CV.Frontend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/auth/login/login/login.component';
-import { RegisterComponent } from './pages/auth/register/register/register.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
