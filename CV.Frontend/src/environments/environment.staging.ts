f70fe3a39d18d27db2b6aba21162fef6376a2de3
asimsca <asimsca@gmail.com>
2025-10-11 04:55:15 +0500

    cv changes

--- CV.Frontend/src/environments/environment.staging.ts
+++ CV.Frontend/src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production : false,
     environmentName : 'staging',
-    baseAPIUrl: 'http://cvapi.runasp.net/api'
+    // baseAPIUrl: 'http://cvapi.runasp.net/api'
+    baseAPIUrl: 'https://cvapi.somee.com/api'    
 }
